16bfa08f8038028047d86db63c0d70bf7f304734
AJ Guiles <ashiguiles@gmail.com>
2019-04-12 06:33:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,18 +4,18 @@ class Artist
  
  
   
-# def initialize(name)
-#   @name = name 
-#   @@songs= []
-# end
+def initialize(name)
+  @name = name 
+  @@songs= []
+end
 
-# def songs 
-#   @@songs
-# end
+def songs 
+  @@songs
+end
 
-# def add_song(song)
-#   song.artist = self
-# end
+def add_song(song)
+  song.artist = self
+end
 
 # def add_song_by_name(name)
 #   song = Song.new(name)

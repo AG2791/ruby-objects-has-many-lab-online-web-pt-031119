af6993f34815c5d223476ce7c2786c7dc57eb6ad
AJ Guiles <ashiguiles@gmail.com>
2019-04-12 06:31:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,58 +1,27 @@
-# class Artist 
+class Artist 
   
-#   attr_accessor :name 
+  attr_accessor :name 
  
-#   @@songs= []
+  @@songs= []
   
-# def initialize(name)
-#   @name = name
-# end
-
-# def songs 
-#   @@songs
-# end
-
-# def add_song(song)
-#   song.artist = self
-# end
-
-# def add_song_by_name(name)
-#   song = Song.new(name)
-#   @songs << song
-#   song.artist = self 
-# end
-
-
-# end
-
-class Artist
-  attr_accessor :name
-
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def add_song(song)
-    song.artist = self
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end
-
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
-
-  def self.song_count
-    Song.all.count
-  end
+def initialize(name)
+  @name = name
 end
+
+def songs 
+  @@songs
+end
+
+def add_song(song)
+  song.artist = self
+end
+
+def add_song_by_name(name)
+  song = Song.new(name)
+  @songs << song
+  song.artist = self 
+end
+
+
+end
+
